57ea4a8f0d7180d77754c4f6129db1230fe1424a
praks0208 <praks.sawant26@gmail.com>
2024-06-13 13:52:50 +0530

    Optimizing React with MillionJS

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
+import MillionLint from "@million/lint";
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [MillionLint.vite()],
   plugins: [react()],
 })
